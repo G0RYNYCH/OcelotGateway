d6825969e520a32cbab514680f7defe542af5074
G0RYNYCH <92940275+GORYNTCHdotNET@users.noreply.github.com>
2022-11-05 22:29:53 +0300

    Add project files.

---
+++ Article.Api/Controllers/ArticlesController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Article.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ArticlesController
+{
+    private readonly IArticleRepository articleRepository;
+    public ArticlesController(IArticleRepository articleRepository)
+    {
+        this.articleRepository = articleRepository;
+    }
+
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok(articleRepository.GetAll());
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult Get(int id)
+    {
+        var article = articleRepository.Get(id);
+        if (article is null)
+            return NotFound();
+
+        return Ok(article);
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult Delete(int id)
+    {
+        var deletedId = articleRepository.Delete(id);
+        if (deletedId == 0)
+            return NotFound();
+
+        return NoContent();
+    }
+}
